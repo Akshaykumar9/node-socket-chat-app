13b186fcd7098267cc067d76c7c5548eb0de0e11
Akshaykumar9 <akshaykumarb18@gmail.com>
2018-07-30 15:46:27 +0000

    add connect and disconnect event handlers

--- server/server.js
+++ server/server.js
@@ -3,9 +3,22 @@ const publicpath=path.join(__dirname, "../public");
 /*console.log(__dirname+"/../public") //this is not recommended
 console.log(publicpath)// this is recommended*/
 
+const http= require("http");// it's a built in node module, which is used by express to create web server
+const socketio=require("socket.io");//it is used to create seemless connection between server and client
 const express=require("express");
 var app= express();
+var server=http.createServer(app);//explictly mentioning this because we want to create a socketio between server and client
+var io= socketio(server);// making http sever to use the socketio and this means we are ready to accept new connections
+
 app.use(express.static(publicpath));
 
-app.listen(process.env.PORT, process.env.IP, () => console.log(`server started on ${process.env.PORT}`))
+io.on('connection', (socket)=>{
+    console.log("New user connected")
+    
+    socket.on('disconnect', ()=>console.log(`client disconnected`));
+    
+}) //io is event listner and connection is a built in event
+
+server.listen(process.env.PORT, process.env.IP, () => console.log(`server started on ${process.env.PORT}`))// we're using server instead of app because we are explicitly creating http server
+
 
